00bcd9ca8038a392d952485c923e25a4d3df92bf
Viktoriia Biriukova <v.biriukova@recman.io>
2026-09-24 13:50:44 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+// import { observer } from "mobx-react";
+
+import "./styles.css";
+import booksRepository from "./Books/Books.repository";
+
+function App() {
+  const [list, setList] = useState([]);
+
+  React.useEffect(() => {
+    async function load() {
+      const books = await booksRepository.getBooks();
+      setList(books);
+    }
+    load();
+  }, []);
+
+  return (
+    <div>
+      {list.map((book, i) => (
+        <div key={i}>
+          {book.author}: {book.name}
+        </div>
+      ))}
+      <button
+        onClick={() => {
+          alert("TBD");
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+const ObservedApp = App;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<ObservedApp />, rootElement);
